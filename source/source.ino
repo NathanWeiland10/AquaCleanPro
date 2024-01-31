475f87e08bb100ff9728dfa6e004813789a91806
Ethan Timpe <ethantimpe3@gmail.com>
2024-01-30 22:36:31 -0500

    added WiFi portal and HTTP POST code

--- source/source.ino
+++ source/source.ino
@@ -1,8 +1,12 @@
 #include "Arduino.h"
 #include <WiFiManager.h>
+#include <HTTPClient.h>
+#include "sensors.hpp"
 
-WifiManager wifiManager; ///< Wifi manager instance used to create WiFi portal
-WifiManagerParameter runtimeParam; ///< Device runtime parameter configured in WiFi portal
+const char* POSTGREST_URL = "http://aquacleanpro.org:3000/water_data"; ///< PostgREST URL for water data table
+
+WiFiManager wifiManager; ///< WiFi manager instance used to create WiFi portal
+WiFiManagerParameter runtimeParam; ///< Device runtime parameter configured in WiFi portal
 
 int totalRuntime = -1; ///< Total time in seconds that the device will run before shutting down. Initialized at -1 until configured from WiFi portal.
 int runtime = 0; ///< Time in seconds that the device has been active.
@@ -15,12 +19,16 @@ void IRAM_ATTR onTimer(){
 
 /// @brief Runs once on start-up
 void setup() {
+    Serial.begin(115200);
     initTimer();
+    initWifiPortal();
 }
 
 /// @brief Main update loop
 void loop() {
     wifiManager.process();
+    httpUpload();
+    delay(1000);
 }
 
 /// @brief Initializes the hardware timer used to calculate device runtime.
@@ -34,12 +42,12 @@ void initTimer(){
 /// @brief Initializes the WiFi portal to obtain network credentials.
 void initWifiPortal(){
     wifiManager.resetSettings(); // Remove in real build
-    wifiManager.setConfigPortalBlocking(false);
+    wifiManager.setConfigPortalBlocking(true);
     const char* runtimeHtml = "<br/><label for='runtime'>Runtime</label><br><input type='radio' name='runtime' value='15' checked> 15 minutes <br><input type='radio' name='runtime' value='30'> 30 minutes <br><input type='radio' name='runtime' value='60'> 1 hour";
     
     new (&runtimeParam) WiFiManagerParameter(runtimeHtml); 
     wifiManager.addParameter(&runtimeParam);
-    wifiManager.setSaveParamCallback(saveParamCallback);
+    wifiManager.setSaveParamsCallback(saveParamCallback);
 
     std::vector<const char *> menu = {"wifi","param","sep","restart","exit"};
     wifiManager.setMenu(menu);
@@ -51,6 +59,17 @@ void initWifiPortal(){
 /// @brief Saves runtime setting from WiFi portal to device
 void saveParamCallback(){
     if(wifiManager.server->hasArg("runtime")) {
-        totalRuntime = stoi(wifiManager.server->arg("runtime"));
+        totalRuntime = wifiManager.server->arg("runtime").toInt();
+    }
+}
+
+/// @brief Uploads the temperature and pH data to PostgREST (currently uploads fake data)
+void httpUpload(){
+    if (WiFi.status() == WL_CONNECTED) { 
+      HTTPClient http;
+      http.begin(POSTGREST_URL);
+      int httpResponseCode = http.POST("{ \"temp\": " + String(10 * sin(runtime)) + ", \"ph\": " + String(10 * cos(runtime)) + " }");
+      Serial.println(httpResponseCode);
+      http.end();
     }
 }
