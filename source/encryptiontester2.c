e1a160391ec3ea308e54b3267b657d73f3f7d3f0
Ethan Heinlein <ethanMH2002@gmail.com>
2024-02-12 19:08:28 -0500

    Reading Info from files

--- source/encryptiontester2.c
+++ source/encryptiontester2.c
@@ -13,53 +13,69 @@
 int main()
 {
     struct AES_ctx ctx;
-    unsigned char testData[] = "[{\"time\":\"2024-01-18T13:35:36.742489\",\"temp\":100.00,\"ph\":7.0}]";
-    // Create padded string divisible by 16
-    size_t paddedLength = strlen(testData) + 16 - (strlen(testData) % 16);
-    unsigned char paddedData[paddedLength];
-    // Copy data to paddedData, but add padding at the end
-    for(int i = 0; i < paddedLength; i++)
-    {
-        if (i < strlen(testData))
-        {
-            paddedData[i] = testData[i];
-        }
-        else
-        {
-            paddedData[i] = 16 - (strlen(testData) % 16);
-        }
-    }
-    // Print status of buffer before encryption
-    printf("Length of buffer: %d\n", strlen(paddedData));
-    printf("Is the string divisible by 16? %d\n", strlen(paddedData) % 16 == 0);
-    printf("\n Raw buffer\n");
-    printf("%s\n", paddedData);
-    fflush(stdout);
-
     // Read from key.txt and iv.txt
     // Hex arrays
-    unsigned char preKey[256];
-    unsigned char preIv[256];
+    unsigned char fileBuffer[256];
     // Processed arrays (char)
+    // TODO, dynamic buffer size
+    unsigned char buffer[128];
     unsigned char key[128];
     unsigned char iv[128];
 
-    FILE* fptr = fopen("key.txt", "r");
-    fgets(preKey, 128, fptr);
+    // Get the buffer
+    FILE* fptr = fopen("buffer.txt", "r");
+    fgets(fileBuffer, 128, fptr);
     fclose(fptr);
-    int c = 0;
-    for(int i = 1; i < sizeof(preKey); i+=2)
+    int d = 0;
+    // The last two characters of the buffer should be the number of padded characters
+    // Convert from hex back to char
+    for(int i = 0; i < 128 - 2; i+=2)
     {
-        key[c] = (char)(preKey[i] + preKey[i-1]);
-        c++;
+       char* hex;
+       __mingw_asprintf(&hex, "%c%c", fileBuffer[i], fileBuffer[i+1]);
+       char c = strtoul(hex, NULL, 16);
+       buffer[d++] = c;
     }
+    // Key
+    fptr = fopen("key.txt", "r");
+    fgets(fileBuffer, 256, fptr);
+    fclose(fptr);
+    d = 0;
+    for(int i = 0; i < 256 - 2; i+=2)
+    {
+       char* hex;
+       __mingw_asprintf(&hex, "%c%c", fileBuffer[i], fileBuffer[i+1]);
+       char c = strtoul(hex, NULL, 16);
+       key[d++] = c;
+    }
+    // IV
     fptr = fopen("iv.txt", "r");
-    fgets(preIv, 128, fptr);
+    fgets(fileBuffer, 256, fptr);
     fclose(fptr);
-    c = 0;
-    for(int i = 1; i < sizeof(preIv); i+=2)
+    d = 0;
+    for(int i = 0; i < 256 - 2; i+=2)
     {
-        iv[c] = (char)(preIv[i] + preIv[i-1]);
-        c++;
+      char* hex;
+      __mingw_asprintf(&hex, "%c%c", fileBuffer[i], fileBuffer[i+1]);
+      char c = strtoul(hex, NULL, 16);
+      iv[d++] = c;
     }
+ 
+    printf("Encrypted buffer:\n");
+    for(int i = 0; i < sizeof(buffer); i++)
+    {
+      printf("%c", buffer[i]);
+    }
+    fflush(stdout);
+    // Form the context
+    AES_init_ctx(&ctx, key);
+    AES_ctx_set_iv(&ctx, iv);
+    AES_CBC_decrypt_buffer(&ctx, buffer, sizeof(buffer));
+    printf("\nDecrypted buffer:\n");
+    for(int i = 0; i < sizeof(buffer); i++)
+    {
+        printf("%c", buffer[i]);
+    }
+    fflush(stdout);
+    return 0;
 }
