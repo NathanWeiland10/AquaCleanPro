1de6f49c1903d5185c6196c980c4e58a56f9a5bb
Ethan Heinlein <ethanMH2002@gmail.com>
2024-02-15 09:40:52 -0500

    Add Makefile

--- source/postgrestest.c
+++ source/postgrestest.c
@@ -1,16 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "fmgr.h"
+#include <postgres.h>
+#include <fmgr.h>
+#include <catalog/pg_type.h>
+#include <utils/builtins.h>
 
 PG_MODULE_MAGIC;
+
+PG_FUNCTION_INFO_V1(TestFunc);
 
 // Run these commands to create C library:
 // gcc -fPIC -c foo.c
 // gcc -shared -o foo.so foo.o
 
 // https://www.postgresql.org/docs/current/xfunc-c.html
-int TestFunc()
+Datum
+TestFunc(PG_FUNCTION_ARGS)
 {
-    printf("Hello from postgres.\n");
-    return 0;
+    PG_RETURN_INT32(0);
 }
