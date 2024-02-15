85d4b17e23cc4ccdd0c961cbb00a362efeae9b65
Ethan Heinlein <ethanMH2002@gmail.com>
2024-02-15 08:28:58 -0500

    Include magic module for postgres

--- source/postgrestest.c
+++ source/postgrestest.c
@@ -1,7 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "fmgr.h"
 
-// gcc -shared -o postgrestest.so -fPIC .\source\postgrestest.c
+PG_MODULE_MAGIC;
+
+// Run these commands to create C library:
+// gcc -fPIC -c foo.c
+// gcc -shared -o foo.so foo.o
+
+// https://www.postgresql.org/docs/current/xfunc-c.html
 int TestFunc()
 {
     printf("Hello from postgres.\n");
