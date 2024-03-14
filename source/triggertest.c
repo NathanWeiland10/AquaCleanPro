b2372079767a900b28ca4fe02597f37db60847a0
Ethan Heinlein <ethanMH2002@gmail.com>
2024-03-14 08:30:04 -0400

    First attempt at Postgres decrypt

--- source/triggertest.c
+++ source/triggertest.c
@@ -9,19 +9,21 @@
 
 #define AES128 1
 
+struct AES_ctx ctx;
+unsigned char* hexKey = "<ENTER 256 HEX KEY HERE>";
+unsigned char* hexIv = "<ENTER 256 HEX IV HERE>";
+
 PG_MODULE_MAGIC;
 
-PG_FUNCTION_INFO_V1(decryptOnInsert);
+PG_FUNCTION_INFO_V1(decryptBuffer);
 
 Datum
-decryptOnInsert(PG_FUNCTION_ARGS)
+decryptBuffer(PG_FUNCTION_ARGS)
 {
     // Get trigger context
     TriggerData *trigdata = (TriggerData *) fcinfo->context;
     HeapTuple   tuple;
     HeapTuple   rettuple;
-    int         attnum = 0;
-    Datum       datumVal;
 
     //Make sure that the function is called from a trigger
     if (!CALLED_AS_TRIGGER(fcinfo))
@@ -30,14 +32,25 @@ decryptOnInsert(PG_FUNCTION_ARGS)
     // Connect to server
     SPI_connect();
 
+    char* buf;
     // Triggered by INSERT event
     if (TRIGGER_FIRED_BY_INSERT(trigdata->tg_event))
     {
-        // Grab the tuple
-        tuple = trigdata->tg_trigtuple;
+        // Grab the argument
+        HeapTupleHeader t = PG_GETARG_HEAPTUPLEHEADER(0);
+        bool isnull;
+        Datum type = GetAttributeByName(t, "type", &isnull);
+        buf = VARDATA(DatumGetVarCharPP(type));
+        int len = VARSIZE(DatumGetVarCharPP(type)) - VARHDRSZ;
+        // Set up the context and decrypt
+        AES_init_ctx_iv(&ctx, key, iv);
+        AES_CBC_decrypt_buffer(&ctx, buf, len)
     }
 
     // Close server connection
     SPI_finish();
-    return PointerGetDatum(tuple);
+    // Return the unencrypted buffer to Postgres
+    PG_RETURN_VARCHAR((VarChar *)buf);
+    // Can insert this buffer into water_data from SQL
+    // If that fails we can look into bringing in a HTTP library and post it that way
 }
