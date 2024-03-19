f68ef9ae17adc39129e44627401c03328be0a61d
Ethan Timpe <ethantimpe3@gmail.com>
2024-03-18 22:59:31 -0400

    added models and battery charge code

---
+++ tests/battery-level/battery.cpp
@@ -0,0 +1,11 @@
+#include "battery.hpp"
+#include "Arduino.h"
+
+Battery::Battery(int pinNum, float volt){
+    pin = pinNum;
+    voltage = volt;
+}
+
+float Battery::getCharge(){
+    return analogRead(pinNum) * (5.0 / (4095.0 / (voltage / 5.0)));
+}
