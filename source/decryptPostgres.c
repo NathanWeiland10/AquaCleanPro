fe35a86fcc9ca89f6c6ad3fdadf19626a568362e
Ethan Heinlein <ethanMH2002@gmail.com>
2024-04-19 14:14:14 -0400

    Add fixes to pg module

--- source/decryptPostgres.c
+++ source/decryptPostgres.c
@@ -13,8 +13,8 @@
 #define AES128 1
 
 struct AES_ctx ctx;
-unsigned char* hexKey = "7777343e484d6f592d360726726f0c300a7471080a0a0f432b3722794b4447486d42044f35023f346d1b461f0368463c0808533c325c67545b0d5b3d00036f33436f48776a416d4d5e33497e2c3c2c56071e4a2744593a445c3578076e3c644d3a3d4074274b36643e3c50032d2b7e3f467614087d4049526d703a1853306454";
-unsigned char* hexIv = "631d463b06693f56325c24030147451b1c2d486c444827384a022d37177c7d10250b0c6404362144563a5c3653062c34463f572c0e3d77011e0d77087d6d1a0c4752744b2a656f776a0147365d370e226f4b29492035522e012014463b6e527d0832402f3735672f2c7226435e192c2558107f04386d7801337e44320f416523";
+unsigned char* hexKey = "<128 bit key>";
+unsigned char* hexIv = "<128 bit iv>";
 
 PG_MODULE_MAGIC;
 
@@ -31,25 +31,24 @@ decryptBuffer(PG_FUNCTION_ARGS)
 
     // Buffer back to char
     unsigned char fileBuffer[257];
-    unsigned char charBuffer[len / 2];
-    // Buffer back to character
+    uint8_t buffer [len / 2];
     strcpy(fileBuffer, buf);
     printf("Encrypted buffer: %s\n", fileBuffer);
     int d = 0;
     for(int i = 0; i < len; i+=2)
     {
-        char hex[2];
-        sprintf(&hex, "%c%c", fileBuffer[i], fileBuffer[i+1]);
-        int c = strtoul(hex, NULL, 16);
-        charBuffer[d++] = (unsigned char)c;
+      char hex[2];
+      sprintf(&hex, "%c%c", fileBuffer[i], fileBuffer[i+1]);
+      int c = strtoul(hex, NULL, 16);
+      buffer[d++] = (uint8_t)c;
     }
 
     // Set up key and iv
-    unsigned char key[128];
-    unsigned char iv[128];
+    unsigned char key[16];
+    unsigned char iv[16];
     strcpy(fileBuffer, hexKey);
     int d = 0;
-    for(int i = 0; i < 256; i+=2)
+    for(int i = 0; i < 32; i+=2)
     {
        char hex[2];
        sprintf(&hex, "%c%c", fileBuffer[i], fileBuffer[i+1]);
@@ -59,7 +58,7 @@ decryptBuffer(PG_FUNCTION_ARGS)
     // IV
     strcpy(fileBuffer, hexIv);
     d = 0;
-    for(int i = 0; i < 256; i+=2)
+    for(int i = 0; i < 32; i+=2)
     {
       char hex[2];
       sprintf(&hex, "%c%c", fileBuffer[i], fileBuffer[i+1]);
@@ -68,7 +67,7 @@ decryptBuffer(PG_FUNCTION_ARGS)
     }
     // Set up the context and decrypt
     AES_init_ctx_iv(&ctx, key, iv);
-    AES_CBC_decrypt_buffer(&ctx, buf, len);
+    AES_CBC_decrypt_buffer(&ctx, buf, len / 2);
     elog(NOTICE, "Decrypted buffer: %s", buf);
 
     // Close server connection
