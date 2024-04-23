119aa1655e9818d157cd3b2627ea62e527c90725
Ethan Timpe <ethantimpe3@gmail.com>
2024-04-22 23:16:46 -0400

    Revert "finalized"
    This reverts commit 2e05e6b4ecec5e69936743d7d32a3a3b6fd4fdf7.

--- source/source.ino
+++ source/source.ino
@@ -44,7 +44,7 @@ float distance;
 /// @brief Interrupt service routine that increments runtime every second.
 void IRAM_ATTR onTimer() {
   runtime++;
-  //steeringSystem.update();
+  steeringSystem.update();
 }
 
 /// @brief Runs once on start-up. Initializes WiFi, ISR, and Serial connection
@@ -59,38 +59,20 @@ void setup() {
 
 /// @brief Main update loop
 void loop() {
-  leftMotor.turnForward();
-  delay(1000);
-  leftMotor.stop();
-  delay(1000);
-  leftMotor.turnBackward();
-  delay(1000);
-  leftMotor.stop();
-  delay(1000);
+  wifiManager.process();
 
-  rightMotor.turnForward();
-  delay(1000);
-  rightMotor.stop();
-  delay(1000);
-  rightMotor.turnBackward();
-  delay(1000);
-  rightMotor.stop();
-  delay(1000);
+  if (runtime >= totalRuntime) {
+    for (int i = 0; i < 8; i++) {
+      phSensor.sample();
+    }
 
-  // wifiManager.process();
+    uploadMeasurements();
 
-  // if (runtime >= totalRuntime) {
-  //   for (int i = 0; i < 8; i++) {
-  //     phSensor.sample();
-  //   }
-
-  //   uploadMeasurements();
-
-  //   steer();
-  // }
-  // else {
-  //   steeringSystem.stop();
-  // }
+    steer();
+  }
+  else {
+    steeringSystem.stop();
+  }
 }
 
 /// @brief Initializes the hardware timer used to calculate device runtime.
